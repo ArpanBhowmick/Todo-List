0b7a2fc9fdd50878da9a59ba00b044d545b9dda8
Arpan Bhowmick <arpanbhowmick28@gmail.com>
2025-09-15 14:22:00 +0530

    ADDED

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,12 @@
 import { useContext, useState } from "react";
-import { AiOutlineDelete } from "react-icons/ai";
+import { AiOutlineDelete, AiTwotoneEdit } from "react-icons/ai";
 import { BsCheckLg } from "react-icons/bs";
 import { TodoContext } from "../store/TodoContext";
-import { IoArrowUndoOutline } from "react-icons/io5";
-import { AiTwotoneEdit } from "react-icons/ai";
+import { IoArrowUndoOutline, IoSaveOutline } from "react-icons/io5";
+import { MdCancel } from "react-icons/md";
+
+
+
 
 const TodoItem = ({ todo }) => {
   const { deleteTodo, editTodo, toggleTodoStatus, showCompletedTab } =
@@ -18,11 +21,7 @@ const TodoItem = ({ todo }) => {
   return (
     <div className="flex flex-col shadow-[0px_3px_5px_rgb(0,0,0,0.5)] mb-2.5">
       <div className="bg-[#414040] flex p-6 pb-3.5 pt-2 justify-between items-center ">
-
-        <div className=" mb-2.5 flex-1">
-        {/* <div className="flex flex-col flex-1 mb-2.5"> */}
-        
-
+        <div className=" mb-0 flex-1">
           {/*           
           <h1 className="font-bold text-green-500 text-2xl m-0 ">
             {todo.title}
@@ -38,8 +37,7 @@ const TodoItem = ({ todo }) => {
                 onChange={(e) =>
                   setEditState({ ...editState, title: e.target.value })
                 }
-                className="mt-2.5 w-full h-10 "
-               
+                className="mt-2.5 h-10 "
               />
               <textarea
                 value={editState.description}
@@ -49,18 +47,17 @@ const TodoItem = ({ todo }) => {
                 className=" mt-2 bg-white text-black p-2.5 "
               />
 
-              <div className="flex mt-2 justify-center">
-                <button
-                  className="bg-green-600 p-1 mr-5"
+              <div className="flex mt-3 justify-center gap-20 ">
+                <IoSaveOutline
+                  className="text-4xl ml-2.5 cursor-pointer hover:text-green-700"
                   onClick={() => {
                     editTodo(todo.id, editState.title, editState.description);
                     setEditState({ ...editState, isEditing: false });
                   }}
-                >
-                  save
-                </button>
-                <button
-                  className="bg-red-600 p-1 ml-5"
+                />
+
+                <MdCancel
+                  className=" text-4xl cursor-pointer hover:text-red-700"
                   onClick={() =>
                     setEditState({
                       isEditing: false,
@@ -68,9 +65,7 @@ const TodoItem = ({ todo }) => {
                       description: todo.description,
                     })
                   }
-                >
-                  cancel
-                </button>
+                />
               </div>
             </div>
           ) : (
@@ -82,23 +77,21 @@ const TodoItem = ({ todo }) => {
               <p className="text-sm text-gray-400 mt-0 pt-2">
                 {todo.description}
               </p>
-            </>
-          )}
 
-          {showCompletedTab ? (
-            <p className="text-sm text-gray-400 mt-0 pt-1">
-              completed on : {new Date(todo.completedAt).toLocaleString()}
-            </p>
-          ) : (
-            <p className="text-sm text-gray-400 mt-0 pt-1">
-              Added on : {new Date(todo.id).toLocaleString()}
-            </p>
+              {showCompletedTab ? (
+                <p className="text-sm text-gray-400 mt-0 pt-1">
+                  completed on : {new Date(todo.completedAt).toLocaleString()}
+                </p>
+              ) : (
+                <p className="text-sm text-gray-400 mt-0 pt-1">
+                  Added on : {new Date(todo.id).toLocaleString()}
+                </p>
+              )}
+            </>
           )}
         </div>
 
         {!editState.isEditing && (
-
-          
           <div className="flex">
             <AiOutlineDelete
               className=" text-4xl cursor-pointer hover:text-red-700"
